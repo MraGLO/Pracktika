9522132211b11fe07d659e5bd1908bb2eea5db1c
MraGLO <94346067+MraGLO@users.noreply.github.com>
2023-06-19 12:39:39 +0300

    Add files via upload

---
+++ Bentley-Grooming Практика Кузичев Марк/scripts/swiper-reg.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    speed: 1500,
+    autoplay:{
+        delay: 15000,
+    }
+  });
